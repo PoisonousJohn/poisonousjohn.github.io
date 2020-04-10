f2cf9ea78f91c4e65b9c27137f1b93c98c0d4dca
Ivan Fateev <ivan@anna.money>
2020-04-10 18:05:50 +0300

    updated build and publish

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 set -e
-jekyll build
+set -x
+if [ ! -d "build" ]; then
+    mkdir build
+    cp -r .git build
+    cd build
+    git reset --hard HEAD
+    git checkout master
+    git pull
+fi
+docker-compose run --rm jekyll jekyll build
+
 cd _site
 rm -f CNAME
 touch CNAME
 echo "fateev.pro" >> CNAME
-rm build-and-publish.sh
 mv index.html index.htm
+
+cd ..
+
+rm -rf build/*
+cp -r _site/* build/
+cd build
+rm build-and-publish.sh
+rm -rf .jekyll-cache
 git add .
 git commit -am 'regenerated files'
-git push
+# git push
