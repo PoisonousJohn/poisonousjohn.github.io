1b8def8faf30735281479702d1f98e9e5b08999f
Ivan Fateev <ivan@anna.money>
2020-04-12 22:17:43 +0300

    add yandex metrika

--- assets/js/headerPixels.js
+++ assets/js/headerPixels.js
@@ -1,9 +1,20 @@
 // GTM
 (function (w, d, s, l, i) {
-	w[l] = w[l] || []; w[l].push({
-		'gtm.start':
-			new Date().getTime(), event: 'gtm.js'
-	}); var f = d.getElementsByTagName(s)[0],
-		j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-			'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    w[l] = w[l] || []; w[l].push({
+        'gtm.start':
+            new Date().getTime(), event: 'gtm.js'
+    }); var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
 })(window, document, 'script', 'dataLayer', 'GTM-N8RNTGQ');
+
+// Yandex.Metrika
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+ym(16940533, "init", {
+    clickmap:true,
+    trackLinks:true,
+    accurateTrackBounce:true
+});
